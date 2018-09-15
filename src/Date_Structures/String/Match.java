d2fd7be14c31ae2b42e60307860acd7dc8ccc05a
machenxing <machenxing@139.com>
2018-09-15 15:49:33 +0800

    朴素模式匹配

---
+++ src/Date_Structures/String/Match.java
@@ -0,0 +1,34 @@
+package Date_Structures.String;
+
+public class Match {
+    public int match(String s , String t , int post){
+        if(s == null || t == null){
+            return -1;
+        }
+        int len1 = s.length();
+        int len2 = t.length();
+        int i = post;
+        int j = 0;
+        while (i < len1 && j < len2) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            }else{
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+        if (j >= len2){
+            return i - len2;
+        }else{
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        String s ="Star is good man";
+        String t =" good";
+        Match matching = new Match();
+        System.out.println(matching.match(s, t, 0));
+        System.out.println(matching.match(s, t, 8));
+    }
+}
