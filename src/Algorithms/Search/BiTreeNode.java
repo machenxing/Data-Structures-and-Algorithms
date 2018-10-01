44b0a41e6d15c8fe307fa82c9f1e64badf08b0d1
machenxing <machenxing@139.com>
2018-10-01 21:02:45 +0800

    二叉树结点

---
+++ src/Algorithms/Search/BiTreeNode.java
@@ -0,0 +1,15 @@
+package Algorithms.Search;
+
+/**
+ * ¶þ²æÊ÷½áµã
+ */
+public class BiTreeNode {
+    int data;
+    BiTreeNode left;
+    BiTreeNode right;
+    public BiTreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
